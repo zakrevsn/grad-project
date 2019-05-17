ff0ac257b52dbc4f0c79d89bcfd3f345845bd97b
Natalia Zakrevskaya <zakrevsn@gmail.com>
2019-05-17 10:53:57 +0200

    Field design fix

--- src/field.js
+++ src/field.js
@@ -27,20 +27,41 @@ class Field extends React.Component {
                 );
             }
         }
-        let caption;
+        let caption = null;
         if (this.props.myTurn == null) {
             if (this.props.myField) {
                 caption = "Place your ships";
             } else {
                 caption = "Legend";
             }
-        } else if (this.props.myTurn) {
         }
         return (
             <div className="field-container">
                 <div className="field-caption">{caption}</div>
-
-                <div className="field">{cells}</div>
+                <div className="field">
+                    <div />
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                    <div>7</div>
+                    <div>8</div>
+                    <div>9</div>
+                    <div>10</div>
+                    <div>a</div>
+                    <div>b</div>
+                    <div>c</div>
+                    <div>d</div>
+                    <div>e</div>
+                    <div>f</div>
+                    <div>g</div>
+                    <div>h</div>
+                    <div>i</div>
+                    <div>j</div>
+                    <div className="cells-container">{cells}</div>
+                </div>
             </div>
         );
     }
